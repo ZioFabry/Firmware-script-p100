b456ffb511e3cb70d5afbc4c3fd31a6e8f0b0fd9
Fabrizio Amodio <ziofabry@hotmail.com>
2023-04-14 10:34:43 +0200

    GW v1.0.2

--- 0.60/EU/update.sh
+++ 0.60/EU/update.sh
@@ -15,9 +15,10 @@ FIRMWARE_VERSION="0.60"
 GATEWAY_RS_PATH="/etc/helium_gateway"
 GATEWAY_REGION="EU"
 GATEWAY_URL="https://github.com/helium/gateway-rs/releases/download"
-GATEWAY_VER="v1.0.0/helium-gateway-1.0.0-armv7-unknown-linux-musleabihf.tar.gz"
+GATEWAY_VER="1.0.2"
+GATEWAY_TAR="helium-gateway-${GATEWAY_VER}-armv7-unknown-linux-musleabihf.tar.gz"
 
-echo "update $FIRMWARE_VERSION"
+echo "Firmware update $FIRMWARE_VERSION"
 
 mkdir -p "$GATEWAY_RS_PATH/"
 echo "🍺 mkdir $GATEWAY_RS_PATH/"
@@ -26,43 +27,47 @@ rm -rf $GATEWAY_RS_PATH/*
 echo "🍺rm -rf $GATEWAY_RS_PATH/*"
 
 # Download the gateway_rs programe
-wget "$GATEWAY_URL/$GATEWAY_VER" -P "$GATEWAY_RS_PATH/"
+wget "$GATEWAY_URL/v$GATEWAY_VER/$GATEWAY_TAR" -P "$GATEWAY_RS_PATH/"
 wait
 # Unzip the pack
-tar -xvf "$GATEWAY_RS_PATH/helium-gateway-1.0.0-armv7-unknown-linux-musleabihf.tar.gz" -C "$GATEWAY_RS_PATH/"
+tar -xvf "$GATEWAY_RS_PATH/$GATEWAY_TAR" -C "$GATEWAY_RS_PATH/"
 wait
 
 # Download config
+echo "🍺 fetch $BASEURL/$FIRMWARE_VERSION/$GATEWAY_REGION/settings.toml -> $GATEWAY_RS_PATH/settings.toml"
 curl -Lf "$BASEURL/$FIRMWARE_VERSION/$GATEWAY_REGION/settings.toml" -o "$GATEWAY_RS_PATH/settings.toml"
 
-echo "🍺 fetch $BASEURL/$FIRMWARE_VERSION/$GATEWAY_REGION/settings.toml to $GATEWAY_RS_PATH/settings.toml"
-
 # Download the service 
+echo "🍺 fetch $BASEURL/$FIRMWARE_VERSION/helium.service -> /lib/systemd/system/helium.service"
 curl -Lf "$BASEURL/$FIRMWARE_VERSION/helium.service" -o "/lib/systemd/system/helium.service"
 
 # Update the init
+echo "🍺 fetch $BASEURL/$FIRMWARE_VERSION/init.sh -> /home/pi/hnt/script/init.sh"
 curl -Lf "$BASEURL/$FIRMWARE_VERSION/init.sh" -o "/home/pi/hnt/script/init.sh"
 
 # Stop miner container if already started
+echo "🍺 Removing old Docker if present..."
     docker stop miner || true 
     docker rm miner || true 
 
+echo "🍺 Installing new service..."
     systemctl daemon-reload
 # Stop the service of helium
-    service helium stop 
+    systemctl stop helium
 
 # Start up the service
-    service helium start
+    systemctl enable helium
+    systemctl start helium
 
-echo "Helium_gateway running and updated"
+echo "🍺Helium_gateway running and updated"
 
 # Update the lsb_release file
-echo "DISTRIB_RELEASE=2023.03.30" | sudo tee /etc/lsb_release
+echo "DISTRIB_RELEASE=gw_$GATEWAY_VER" | sudo tee /etc/lsb_release
 wait
-echo "version update"
+echo "🍺GW version $GATEWAY_VER updated"
 wait
 
 # Update the version file
 curl -Lf $BASEURL/$FIRMWARE_VERSION/version -o /home/pi/api/tool/version;
 wait
-echo "update $FIRMWARE_VERSION success"
+echo "🍺Firmware $FIRMWARE_VERSION updated"
